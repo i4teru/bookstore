2447285a71c2e86022c83717e67f101770f77455
wramkim <wramkim@kakao.com>
2020-04-20 18:10:52 +0900

    메인화면, 로고 등 수정

--- sts0417/src/main/java/net/bs/spring/HomeController.java
+++ sts0417/src/main/java/net/bs/spring/HomeController.java
@@ -30,4 +30,11 @@ public class HomeController {
 		return "home";
 	}
 	
+	//main.do
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	public String mainDo(Model model) {
+		
+		return "main";
+	}
+	
 }
